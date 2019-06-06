7054aa2e72ead7e0826cb5313752eee327e510f1
Filya15072004 <51395761+Filya15072004@users.noreply.github.com>
2019-06-06 14:18:55 +0900

    Add files via upload

---
+++ Scripts/Enemy.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+public class Enemy : MonoBehaviour
+{
+	public Transform player;
+  public NavMeshAgent agent;
+  public Text scoreText;
+  static int score;
+  	
+ 	Rigidbody rbenemy;
+
+    void Start()
+    {
+      rbenemy = GetComponent<Rigidbody>();
+      score = 0;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+      agent.SetDestination(player.transform.position);  
+    }
+    void OnCollisionEnter(Collision other)
+    {
+      if(other.gameObject.tag == "Bullet"){
+
+        score++;
+        scoreText.text = "Score " + score;
+
+        Destroy(gameObject);
+        if(score >= 4){
+          SceneManager.LoadScene(0);
+          score = 0;
+        }
+
+      }
+
+    }
+}
---
+++ Scripts/Player.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+public class Player : MonoBehaviour
+{
+    public Text zdText;
+    int zd;
+    Rigidbody rb;
+    public GameObject bullet;
+    Rigidbody rbbullet;
+	GameObject clonebul;
+
+    void Start()
+    {
+        zd = 5;
+       rb = GetComponent<Rigidbody>();
+    }
+
+   
+    void Update()
+    {
+        float mVertical = Input.GetAxis("Vertical");
+        rb.AddForce(transform.forward * mVertical * 15f);
+
+        float mHorizontal = Input.GetAxis("Horizontal") * 5f;
+        transform.Rotate(0,mHorizontal,0);
+
+		if(Input.GetKeyDown("space")){
+		 clonebul = Instantiate(bullet, new Vector3(transform.position.x, transform.position.y, transform.position.z), Quaternion.identity);
+		  rbbullet = clonebul.GetComponent<Rigidbody>();
+            rbbullet.AddForce(transform.forward * 800f);
+		}      
+    }
+    void OnCollisionEnter(Collision player){
+        if(player.gameObject.tag == "Enemy"){
+             zd--;
+
+            zdText.text = "HP " + zd;
+
+            if(zd <= 0){
+                SceneManager.LoadScene(1);
+                zd = 5;
+            }
+        }
+    }
+
+}
